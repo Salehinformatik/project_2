2eb8edddf7d0914e02e57a88279d87a0d4aa1e55
saleh <saleh.informatik@gmail.com>
2022-10-15 00:00:30 +0200

    Add project to repo first time

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+     byte b1 = -128;
+     short s1 = 20;
+     int i1 = 100;
+     long l1 = 99;
+     float f1 = 444.3F;
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE) ;
+        System.out.println(i1-l1);
+        System.out.println(f1);
+    }
+}
